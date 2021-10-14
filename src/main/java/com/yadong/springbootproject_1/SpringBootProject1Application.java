24fadec4f28594a7a4d1cde5b82550b7bd179d85
YadongTan <2209931449@qq.com>
2021-10-14 13:25:52 +0800

    push test

--- src/main/java/com/yadong/springbootproject_1/SpringBootProject1Application.java
+++ src/main/java/com/yadong/springbootproject_1/SpringBootProject1Application.java
@@ -2,12 +2,13 @@ package com.yadong.springbootproject_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootProject1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootProject1Application.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(SpringBootProject1Application.class, args);
     }
 
 }
