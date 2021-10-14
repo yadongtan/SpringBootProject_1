89a724f0f65ac4649e9034e4c83b784b295dfb97
YadongTan <2209931449@qq.com>
2021-10-15 00:48:19 +0800

    完成了未登录拦截跳转功能,配置了Druid数据库连接池

--- src/main/java/com/yadong/springbootproject_1/SpringBootProject1Application.java
+++ src/main/java/com/yadong/springbootproject_1/SpringBootProject1Application.java
@@ -1,9 +1,13 @@
 package com.yadong.springbootproject_1;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @MapperScan("com.yadong.springbootproject_1.dao")
@@ -11,6 +15,8 @@ public class SpringBootProject1Application {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(SpringBootProject1Application.class, args);
+        DataSource dataSource = (DataSource) run.getBean("dataSource");
+        System.out.println(dataSource);
     }
 
 }
---
+++ src/main/java/com/yadong/springbootproject_1/interceptor/LoginInterceptor.java
@@ -0,0 +1,30 @@
+package com.yadong.springbootproject_1.interceptor;
+
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (request.getSession().getAttribute("username") == null) {
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("application/json;charset=utf-8");
+            response.sendRedirect("/index.html");
+            return false;
+        }
+        return true;
+    }
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+    }
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+    }
+}
